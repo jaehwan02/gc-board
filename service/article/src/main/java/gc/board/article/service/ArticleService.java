918d5ec08d79c747eace5d79b516d334ff146049
Jaehwan Lee <jhrick0129@gmail.com>
2025-09-10 01:28:43 +0900

    article service

--- service/article/src/main/java/gc/board/article/service/ArticleService.java
+++ service/article/src/main/java/gc/board/article/service/ArticleService.java
@@ -4,17 +4,22 @@ import gc.board.article.entity.Article;
 import gc.board.article.repository.ArticleRepository;
 import gc.board.article.service.request.ArticleCreateRequest;
 import gc.board.article.service.request.ArticleUpdateRequest;
+import gc.board.article.service.response.ArticlePageResponse;
 import gc.board.article.service.response.ArticleResponse;
 import kuke.board.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ArticleService  {
   private final Snowflake snowflake = new Snowflake();
   private final ArticleRepository articleRepository;
+  private static final Long MOVABLE_PAGE_COUNT = 10L;
+
 
   @Transactional
   public ArticleResponse create(ArticleCreateRequest request) {
@@ -39,5 +44,21 @@ public class ArticleService  {
   @Transactional
   public void delete(Long articleId) {
     articleRepository.deleteById(articleId);
+  }
+
+  public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
+
+    Long offset = calculateOffset(page, pageSize);
+    List<Article> articles = articleRepository.findAll(boardId, offset, pageSize);
+    List<ArticleResponse> articleResponses = articles.stream()
+            .map(ArticleResponse::from)
+            .toList();
+    Long limit = PageLimitCalculator.calculatePageLimit(page, pageSize, MOVABLE_PAGE_COUNT);
+    Long articleCount = articleRepository.count(boardId, limit);
+    return ArticlePageResponse.of(articleResponses, articleCount);
+  }
+
+  private Long calculateOffset(Long page, Long pageSize) {
+    return (page - 1) * pageSize;
   }
 }
